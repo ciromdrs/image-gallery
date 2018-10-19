ff98a32bb7106d352dee0dbfdf80c5347af59dba
Ciro <ciromoraismedeiros@gmail.com>
2018-10-19 18:05:03 -0300

    Save Photo info to database

--- galleryproj/gallery/models.py
+++ galleryproj/gallery/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Photo(models.Model):
     label = models.CharField(max_length=200)
+    s3url = models.URLField(default='')
+
+    def __str__(self):
+        return self.label+":"+self.s3url
     
--- galleryproj/gallery/views.py
+++ galleryproj/gallery/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 from . import s3lib
+from . import models
 
 def index(request):
     return render(request, 'home.html')
@@ -15,11 +16,11 @@ def sign_s3(request):
 
 def submit_form(request):
     # Collect the data posted from the HTML form in upload.html:
-    username = request.POST["username"]
-    full_name = request.POST["full-name"]
-    aws_img_url = request.POST["img-url"]
+    label = request.POST["label"]
+    s3url = request.POST["img-url"]
  
     # Provide some procedure for storing the new details
+    models.Photo(label=label,s3url=s3url).save()
 
     # Redirect to the user's profile page, if appropriate
     return HttpResponseRedirect(reverse('home'))#, args=(question.id,)))
