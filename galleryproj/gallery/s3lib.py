acf7e2088fcc49a5cb3ab5284e2111ee9fbcbee2
Ciro <ciromoraismedeiros@gmail.com>
2018-10-19 16:29:00 -0300

    Project scaffold plus s3 file upload

---
+++ galleryproj/gallery/s3lib.py
@@ -0,0 +1,20 @@
+import json, boto3, os
+
+def sign(file_name, file_type):
+    S3_BUCKET = os.environ['BUCKET']
+    s3 = boto3.client('s3')
+    presigned_post = s3.generate_presigned_post(
+    Bucket = S3_BUCKET,
+    Key = file_name,
+    Fields = {"acl": "public-read", "Content-Type": file_type},
+    Conditions = [
+        {"acl": "public-read"},
+        {"Content-Type": file_type}
+    ],
+    ExpiresIn = 3600
+    )
+
+    return json.dumps({
+      'data': presigned_post,
+      'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+    })
