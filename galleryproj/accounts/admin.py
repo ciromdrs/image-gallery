12656ef8f49b7100ec83d59b35465accea43adb0
Ciro <ciromoraismedeiros@gmail.com>
2018-10-19 20:19:10 -0300

    User sign-up

--- galleryproj/accounts/admin.py
+++ galleryproj/accounts/admin.py
@@ -1,5 +1,28 @@
-from django.contrib import admin
+'''from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserAdmin)'''
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+#  from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username',)
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+admin.site.register(get_user_model(), CustomUserAdmin)
--- galleryproj/accounts/views.py
+++ galleryproj/accounts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
-from django.contrib.auth.forms import UserCreationForm
+from .admin import CustomUserCreationForm
 from django.urls import reverse_lazy
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             group = Group.objects.get(name='friends')
@@ -15,5 +15,5 @@ def signup(request):
             print(user.groups)
             return redirect('login')
     else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
