c4e8c10ac73a6a51e6adb3145778060ae0814cb2
Ciro <ciromoraismedeiros@gmail.com>
2018-10-19 18:16:51 -0300

    List user uploaded photos

--- galleryproj/gallery/urls.py
+++ galleryproj/gallery/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
     path('sign-s3/', views.sign_s3, name='sign-s3'),
     path('submit-form/', views.submit_form, name='submit'),
     path('upload/', views.upload, name='upload'),
--- galleryproj/gallery/views.py
+++ galleryproj/gallery/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 from . import s3lib
-from . import models
+from .models import Photo
 
-def index(request):
+def home(request):
     return render(request, 'home.html')
 
 def sign_s3(request):
@@ -15,15 +15,13 @@ def sign_s3(request):
     return HttpResponse(s3lib.sign(filename,filetype))
 
 def submit_form(request):
-    # Collect the data posted from the HTML form in upload.html:
     label = request.POST["label"]
     s3url = request.POST["img-url"]
  
-    # Provide some procedure for storing the new details
-    models.Photo(label=label,s3url=s3url).save()
+    Photo(label=label,s3url=s3url).save()
 
-    # Redirect to the user's profile page, if appropriate
     return HttpResponseRedirect(reverse('home'))#, args=(question.id,)))
 
 def upload(request):
-    return render(request, 'upload.html')
+    photos = Photo.objects.all() # TODO: filter photos by user
+    return render(request, 'upload.html', {'photos':photos})
