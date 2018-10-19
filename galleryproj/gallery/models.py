06af8120d4a8972b14ee16e2051d12e93429c4ec
Ciro <ciromoraismedeiros@gmail.com>
2018-10-19 17:46:23 -0300

    Create Photo model

--- galleryproj/gallery/models.py
+++ galleryproj/gallery/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    label = models.CharField(max_length=200)
+    
--- galleryproj/galleryproj/settings.py
+++ galleryproj/galleryproj/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallery.apps.GalleryConfig',
 ]
 
 MIDDLEWARE = [
