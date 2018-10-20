8758ca670f531a0acc12f306a9d982b3e11c4a01
Ciro <ciromoraismedeiros@gmail.com>
2018-10-20 08:57:01 -0300

    S3 Tests

--- galleryproj/gallery/tests.py
+++ galleryproj/gallery/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 
+import json
+
+from . import s3lib
+
 from .models import Photo, Like
 
 class LikeModelTests(TestCase):
@@ -9,3 +13,13 @@ class LikeModelTests(TestCase):
 class PhotoModelTests(TestCase):
     def test_something(self):
         self.assertIs(True, True)
+
+class S3Tests(TestCase):
+    def test_sign(self):
+        signature_str = s3lib.sign('hello','text')
+        self.assertIs(type(signature_str)==str, True)
+        
+        signature = json.loads(signature_str)
+        self.assertIs(type(signature)==type(dict()), True)
+        self.assertIs(type(signature['url'])==type(''), True)
+        self.assertIs(type(signature['data'])==type(dict()), True)
